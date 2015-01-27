757ab9d5ab2f299b3d3a14962cb2f3892ecb16ad
PJBentham <PJBentham@gmail.com>
2015-01-27 22:44:18 +0000

    Added Game class allowing creation of players (always including dealer), awaiting answer on SO why NoneType error occuring in number_of_players() method

--- Blackjack.py
+++ Blackjack.py
@@ -31,17 +31,55 @@ class Player():
 class Dealer(Player):	
 	pass
 
+class Game():
+	def number_of_players(self):
+		try:
+			no_of_players = int(raw_input("How many players would you like?"))
+		except NameError and ValueError:
+			return self.number_of_players()
+		if no_of_players>0:	
+			return no_of_players	
+		else:
+			self.number_of_players()	
+
+	def create_players(self, no_of_players, deck):
+		players=[]
+		for player in range(0, no_of_players):
+			players.append(Player(deck))
+		dealer = self.create_dealer(deck)	
+		return players, dealer
+
+	def create_dealer(self, deck):
+		return Dealer(deck)		
+		
+
 if __name__ == "__main__":
+	# Create a deck instance
 	deck = Deck()
+	
+	# Create an ordered deck using Deck() instance
 	ordered_deck = deck.add_deck()
+
+	#Shuffle the ordered deck to represent real life
 	shuffled_deck = deck.shuffle_deck(ordered_deck)
 
-	print shuffled_deck	
-	dealer = Dealer(shuffled_deck)
-	player = Player(shuffled_deck)
+	#Create a new game instance
+	game = Game()
 
-	for i in range(0, 13):
-		print dealer.get_card()
-		print player.get_card()
+	#Request the number of players for the game
+	no_of_players = game.number_of_players()
 
-	print shuffled_deck
+	#Add instances of those players as Players(), 
+	#create the dealer & 
+	#give them all the deck to use
+	players, dealer = game.create_players(no_of_players, shuffled_deck)
+
+	#Get a card for each player in the game and print
+	for i in range(0, len(players)):
+		print players[i].get_card()
+	print dealer.get_card()	
+	# for i in range(0, 13):
+	# 	print dealer.get_card()
+	# 	print player.get_card()
+
+	# print shuffled_deck
